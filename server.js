3117ca452f044b0ad35f3ee93042f9ab0355dc5e
Susan Subedi <susansubedi34@gmail.com>
2020-01-07 16:20:11 -0600

    Prepare for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const userRouter = require("./routes/api/users");
 const authRouter = require("./routes/api/auth");
 const profileRouter = require("./routes/api/profile");
 const postsRouter = require("./routes/api/posts");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -19,6 +20,14 @@ app.use("/users", userRouter);
 app.use("/posts", postsRouter);
 app.use("/profile", profileRouter);
 app.use("/auth", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server running on PORT " + PORT);
